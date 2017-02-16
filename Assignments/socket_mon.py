a1650cf357322da39a3c12ec84a8b1ff35ad764f
poojithaamin <poojaamin87@gmail.com>
2017-02-16 11:40:36 -0800

    Update socket_mon.py

--- Assignments/socket_mon.py
+++ Assignments/socket_mon.py
@@ -14,9 +14,11 @@ for p in psutil.net_connections(kind='tcp'):
         raddr_var = str(p.raddr)
         raddr_var = raddr_var.replace(',', '@')
         raddr_var = raddr_var.translate(None, "()'' ")
-        conn_var = [p.pid, laddr_var, raddr_var, p.status]
+        conn_var = (p.pid, laddr_var, raddr_var, p.status)
         conn_list.append(conn_var)
 
+#Implement without using group by
+"""
 #Get number of connections per process
 freq = Counter(process[0] for process in conn_list)
 
@@ -27,5 +29,18 @@ sorted_list = sorted(conn_list, key=lambda i: freq[i[0]], reverse=True)
 #Print the sorted list
 print '\"pid\",\"laddr\",\"raddr\",\"status\"'
 for field in sorted_list:
+    print '"%s","%s","%s","%s"' %(field[0], field[1], field[2], field[3])
+"""
+
+#Implement using group by
+conn_list.sort(key = itemgetter(0))
+
+#Group by PID and sort the output by the number of connections
+group_list = [list(v) for k,v in groupby(conn_list, key = itemgetter(0) )]
+group_list.sort(key = len, reverse=True)
+
+group_list=list( chain.from_iterable(group_list))
+print '\"pid\",\"laddr\",\"raddr\",\"status\"'
+for field in group_list:
     print '"%s","%s","%s","%s"' %(field[0], field[1], field[2], field[3])
 
